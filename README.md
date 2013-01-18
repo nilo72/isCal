8d35a9d7c4cefb55b73de379ee61076cffe5cd80
nilo <nilo@nilopro.informatik.haw-hamburg.de>
2013-01-18 15:37:53 +0100

    Simple Model evaluated...

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ iCal
 ====
 
 Implementation of RFC 2445 iCalendar
+
+this is a first try of a scala project
---
+++ src/ical/Main.scala
@@ -0,0 +1,12 @@
+package ical
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+   val event = VEvent(List(Location(value="PC-Pool 11.01 a"),Organizer(value="Oliver Neumann"),Description(value="BAI1-PRP1")))
+   val event2 = VEvent(List(Location(value="PC-Pool 11.01 a"),Organizer(value="Oliver Neumann"),Description(value="BAI1-PRP1")))
+   val icalbody = ICalbody(List(event,event2))
+   val cal = ICalobject(body=icalbody)
+   println(cal.toString)
+  }
+}
